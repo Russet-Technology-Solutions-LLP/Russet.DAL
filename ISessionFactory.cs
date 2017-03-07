30011516d0a952ce34039bfd0e31325fc2c3cbc8
Sandeep Kumar <kumarsandeep91@gmail.com>
2017-03-07 20:31:23 +0530

    Initial commit. data access layer setup.

---
+++ ISessionFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace Russet.DAL
+{
+    public interface ISessionFactory
+    {
+        ///<summary>
+        /// Gets the current UoW.
+        /// </summary>
+        /// <value>The current UoW.</value>
+        IUnitOfWork CurrentUoW { get; }
+    }
+}
